93dd5571cc910fd9af320139859cf3bfa9ed51e5
Christian Claus <ch.claus@me.com>
2015-11-24 22:57:15 +0100

    Added another Verticle to scrape a webpage.

--- src/main/java/de/chclaus/demo/vertx/Microservice.java
+++ src/main/java/de/chclaus/demo/vertx/Microservice.java
@@ -1,13 +1,11 @@
 package de.chclaus.demo.vertx;
 
+import de.chclaus.demo.vertx.scraper.ChefkochScraper;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
-import io.vertx.core.http.HttpClient;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
 /**
  * @author chclaus (ch.claus@me.com)
@@ -16,6 +14,8 @@ public class Microservice extends AbstractVerticle {
 
   @Override
   public void start(Future<Void> fut) throws Exception {
+    vertx.deployVerticle(new ChefkochScraper());
+
     Router mainRouter = Router.router(vertx);
     mainRouter.route().handler(BodyHandler.create());
 
@@ -45,23 +45,10 @@ public class Microservice extends AbstractVerticle {
 
     // atm static urls...
     url = "/rezepte/1108021216887354/Flammkuchen-elsaesser-Art-suesser-Flammkuchen.html";
-
-    HttpClient httpClient = vertx.createHttpClient();
-    httpClient.getNow("www.chefkoch.de", url, httpClientResponse -> {
-      httpClientResponse.bodyHandler(buffer -> {
-        String html = buffer.toString();
-        Document document = Jsoup.parse(html);
-        Recipe recipe = new Recipe();
-
-        document.select(".incredients .ingredient .name").stream().forEach(ingredient -> {
-          recipe.getIngredients().put(ingredient.text(), ingredient.previousElementSibling().text());
-        });
-
-        recipe.setPrepTime(document.select(".prepTime").text())
-            .setText(document.select("#rezept-zubereitung").text());
-
-        routingContext.response().end(recipe.toString());
-      });
+    vertx.eventBus().send("chefkoch", url, reply -> {
+      if (reply.succeeded()) {
+        routingContext.response().end(reply.result().body().toString());
+      }
     });
   }
 }
--- src/main/java/de/chclaus/demo/vertx/scraper/ChefkochScraper.java
+++ src/main/java/de/chclaus/demo/vertx/scraper/ChefkochScraper.java
@@ -1,11 +1,51 @@
 package de.chclaus.demo.vertx.scraper;
 
+import de.chclaus.demo.vertx.Recipe;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpClient;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 /**
  * @author chclaus (ch.claus@me.com)
  */
 public class ChefkochScraper extends AbstractVerticle implements Scraper {
+
+  @Override
+  public void start(Future<Void> startFuture) throws Exception {
+    vertx.eventBus().consumer("chefkoch", message -> {
+      String url = (String) message.body();
+
+      HttpClient httpClient = vertx.createHttpClient();
+      httpClient.getNow("www.chefkoch.de", url, httpClientResponse -> {
+        httpClientResponse.bodyHandler(buffer -> {
+          String html = buffer.toString();
+          Document document = Jsoup.parse(html);
+          Recipe recipe = new Recipe();
+
+          document.select(".incredients .ingredient .name").stream().forEach(ingredient -> {
+            recipe.getIngredients().put(ingredient.text(), ingredient.previousElementSibling().text());
+          });
+
+          recipe.setPrepTime(document.select(".prepTime").text())
+              .setText(document.select("#rezept-zubereitung").text());
+
+          message.reply(recipe.toString());
+        });
+      });
+    });
+
+
+
+
+
+
+
+
+
+
+  }
 
   @Override
   public String scrape() {
